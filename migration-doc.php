b07ce43eefb82d487456ce553fbba4e0cab88a08
solaymanWeb <solaymanweb27@gmail.comm>
2023-06-05 07:43:51 +0600

    e-commerce table create

--- migration-doc.php
+++ migration-doc.php
@@ -1,1 +1,12 @@
 <!--E-commerce migration table create -->
+
+php artisan make:migration create_users
+php artisan make:migration create_profiles
+php artisan make:migration create_categories
+php artisan make:migration create_brands
+php artisan make:migration create_products
+php artisan make:migration create_product_details
+php artisan make:migration create_product_sliders
+php artisan make:migration create_product_reviews
+php artisan make:migration create_product_wishes
+php artisan make:migration create_product_carts
